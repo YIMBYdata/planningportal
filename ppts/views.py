d2d835fef09ca6d7d289ae35c5e60a868965f5cd
Elliot Reed <tristanlmiller@gmail.com>
2019-07-04 15:53:55 -0700

    add graph using database

--- ppts/views.py
+++ ppts/views.py
@@ -5,13 +5,24 @@ from django.http import Http404
 import matplotlib.pyplot as plt
 import PIL, PIL.Image
 import io
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+from ppts.models import DwellingType
+from ppts.models import LandUse
+from ppts.models import Location
+from ppts.models import Planner
+from ppts.models import ProjectFeature
+from ppts.models import ProjectDescription
+from ppts.models import Record
+from ppts.models import RecordType
+from django.db.models import Count
 
 def index(request):
     graph_list = []
     
     #append names of graphs
     graph_list.append('samplegraph')
-    graph_list.append('samplegraph')
+    graph_list.append('samplegraph2')
     graph_list.append('samplegraph')
     
     #use list of graphs as context and render page
@@ -20,27 +31,44 @@ def index(request):
 
 def graphmanager(request, graphname):
     '''given the name of the graph, sends request to appropriate function'''
-    graph_view = globals()['graph_' + graphname]
-    return graph_view(request)
+    graph_func = globals()['graph_' + graphname]
+    return figureToHttp(graph_func())
     #to be added: 404 error when there's no graph
 
-def graph_samplegraph(request):
+def graph_samplegraph():
     # Construct the graph
+    fig,ax = plt.subplots()
     x = range(0, 20, 1)
     s = range(0,40,2)
-    plt.plot(x, s)
+    ax.plot(x, s)
 
-    plt.xlabel('xlabel(X)')
-    plt.ylabel('ylabel(Y)')
-    plt.title('Sample Graph')
+    ax.set_xlabel('xlabel(X)')
+    ax.set_ylabel('ylabel(Y)')
+    ax.set_title('Sample Graph')
     
-    fig = plt.gcf()
-    return figureToHttp(fig)
+    return fig
+
+def graph_samplegraph2():
+    #a basic graph that uses ppts data
+    fig,ax = plt.subplots()
+    
+    projects = Record.objects.filter(record_type__pk = 'PRJ').values('status').annotate(status_counts = Count('pk'))
+    status_counts = []
+    status = []
+    for item in projects:
+        status.append(item['status'])
+        status_counts.append(item['status_counts'])
+    
+    ax.pie(status_counts, labels=status)
+    
+    return fig
     
 def figureToHttp(figure):
     '''Stores a figure as bytes, and then returns an HttpResponse.'''
     buffer = io.BytesIO()
-    canvas = figure.canvas
+    
+    canvas=FigureCanvas(figure)
+    #canvas = figure.canvas
     canvas.draw()
     
     pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
