2ed0721bf2e93d28e3635527ba777a6765ccc227
Elliot Reed <tristanlmiller@gmail.com>
2019-07-04 11:40:04 -0700

    add line plot

--- ppts/urls.py
+++ ppts/urls.py
@@ -4,4 +4,5 @@ from ppts import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('test', views.getimage, name='testgraph'),
 ]
--- ppts/views.py
+++ ppts/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+import matplotlib.pyplot as plt
+import PIL, PIL.Image
+import io
+
 def index(request):
     context = {}
     return render(request, 'ppts/index.html', context)
+
+def getimage(request):
+    # Construct the graph
+    x = range(0, 20, 1)
+    s = range(0,40,2)
+    plt.plot(x, s)
+
+    plt.xlabel('xlabel(X)')
+    plt.ylabel('ylabel(Y)')
+    plt.title('Simple Graph!')
+    #plt.grid(True)
+    
+    buffer = io.BytesIO()
+    canvas = plt.gca().figure.canvas
+    #canvas = pylab.get_current_fig_manager().canvas
+    canvas.draw()
+    
+    pilImage = PIL.Image.frombytes("RGB", canvas.get_width_height(), canvas.tostring_rgb())
+    pilImage.save(buffer, "PNG")
+    #pylab.close()
+    
+    return HttpResponse(buffer.getvalue(), content_type="image/png")
